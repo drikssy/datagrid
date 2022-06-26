6a0de790385c29f8ea729adb6970def41af1885f
cedric ngakam <cedricnga2@gmail.com>
2022-06-26 12:53:50 +0200

    firebase setup

---
+++ src/db/firebase/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyD3YgBVhOpMBz_y9hMuMfGWEc4EZ9PkV8k",
+    authDomain: "dedalo-preprod.firebaseapp.com",
+    projectId: "dedalo-preprod",
+    storageBucket: "dedalo-preprod.appspot.com",
+    messagingSenderId: "26733282715",
+    appId: "1:26733282715:web:1de88a2c15cf5fd60c8d3e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
